c32bea090564b4380c3b5a53bf67dff433e6404e
NotDuy <duytruong56@gmail.com>
2020-05-07 19:32:01 -0500

    A Repository of Machine Learning projects

---
+++ TensorFlow_Python/tensorflow.py
@@ -0,0 +1,39 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# %%
+data = tf.keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_label) = data.load_data()
+
+class_names = ['T-shirt/top','Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Bag', 'Ankle boot']
+
+train_images = train_images/255.0
+test_images = test_images/255.0
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape = (28,28)),
+    keras.layers.Dense(128, activation = "relu"),
+    keras.layers.Dense(10, activation = "softmax")
+])
+
+model.compile(optimizer = "adam", loss = "sparse_categorical_crossentropy", metrics=['accuracy'])
+
+model.fit(train_images, train_labels, epoch =5)
+
+prediction = model.predict(test_images)
+
+for i in range(5):
+    plt.grid(False)
+    plt.imshow(test_images[i], cmap=plt.binary)
+    plt.xlabel("Actual: " + class_names[test_labels[i]])
+    plt.title("Prediction" + class_names[np.argmax(prediction[i])])
+    plt.show()
+# %%
+
+
